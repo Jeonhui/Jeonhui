e09bd113406f4f3bb9d1ec1e722b586d0d33f3ae
Jeonhui <dlwjsgml02@naver.com>
2022-04-29 18:28:43 +0900

    covid19

---
+++ covid19.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as Soup
+import time
+from github import Github
+import os
+
+
+def covid19InfoDays():
+    soup = Soup(requests.get(
+        'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=covid').text,
+                'html.parser')
+    info = soup.find('div', {'class': 'status_info'})
+    content = info.text + "\n"
+    return content
+
+
+if __name__ == "__main__":
+    access_token = os.environ['MY_GITHUB_TOKEN']
+    repository_name = "github-action-with-python"
+    now = time.localtime()
+    content = "%04d/%02d/%02d %02d:%02d:%02d" % (
+        now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec) + '\n' + covid19InfoDays()
+    Github(access_token).get_user().get_repo(repository_name).create_issue(title='covid19', body=content)
