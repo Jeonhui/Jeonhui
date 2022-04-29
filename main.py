d8511a022c84d774b8846a2283eb2ccb3c06aa4f
Jeonhui <dlwjsgml02@naver.com>
2022-04-29 16:51:21 +0900

    test

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from scraping import IOSDeveloperNewsScraping
 
 if __name__ == "__main__":
     access_token = os.environ['MY_GITHUB_TOKEN']
-    f = open('README.md', 'w')
+    f = open('./README.md', 'w')
     f.write(IOSDeveloperNewsScraping())
     f.close()
